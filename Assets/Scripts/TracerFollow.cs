97cbf92b6bac379675396e65db6d777b6eb20362
amandeep <amandeepmalhi21@hotmail.com>
2019-10-06 15:14:37 -0700

    Added current project
    current scenes with player movement, object manipulation and shape tracing

---
+++ Assets/Scripts/TracerFollow.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TracerFollow : MonoBehaviour
+{
+
+    public GameObject plane;
+    Animator anim;
+    public string animationName;
+
+    private LineRenderer line;
+    private GameObject wand;
+
+    private float startTime = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+        //anim = 
+        //anim.Stop(animationName, 0, 0.25f);
+        //anim.StopPlayback();
+        //anim.StartPlayback();
+
+        //anim.GetCurrentAnimatorStateInfo(0);
+
+
+
+        line = gameObject.AddComponent<LineRenderer>();
+        line.positionCount = 2;
+        line.startWidth = 0.008f;
+        line.endWidth = line.startWidth;
+        line.startColor = Color.white;
+        line.endColor = Color.white;
+        line.material = new Material(Shader.Find("Unlit/Texture"));
+
+        //player = GameObject.Find("");
+        wand = GameObject.FindGameObjectWithTag("Wand");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        line.SetPosition(0, this.transform.position);
+        line.SetPosition(1, wand.transform.position);
+
+
+        //show current speed
+        //float sped =  anim.GetCurrentAnimatorStateInfo(0).speed;
+        ////print(sped);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        //anim.Play("");
+        if (other.gameObject.tag == "Wand")
+        {
+            print("Wand Connected");
+            //anim.Play(animationName);
+            //anim.StartPlayback();
+            anim.StopPlayback();
+        }
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Wand")
+        {
+            //print("STAY!");
+            //start counter
+            startTime += Time.deltaTime;
+            //Debug.Log(startTime);
+
+            float sped = anim.GetCurrentAnimatorStateInfo(0).speed;
+            print(sped);
+
+            if (startTime < 10)
+            {
+                anim.speed = 1f;
+                sped = anim.GetCurrentAnimatorStateInfo(0).speed;
+                print(sped);
+            } else if (startTime < 15)
+            {
+                anim.speed = 2f;
+                sped = anim.GetCurrentAnimatorStateInfo(0).speed;
+                print(sped);
+            } else if (startTime < 20)
+            {
+                anim.speed = 3f;
+                sped = anim.GetCurrentAnimatorStateInfo(0).speed;
+                print(sped);
+            } else if (startTime < 25)
+            {
+                anim.StartPlayback();
+                print("End Animation");
+                Destroy(plane);
+            }
+        }
+    }
+
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Wand")
+        {
+            print("Wand Exit");
+            //anim.StopPlayback();
+            anim.StartPlayback();
+
+        }
+    }
+}
---
+++ Assets/Scripts/TracerPlane.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TracerPlane : MonoBehaviour
+{
+    
+    
+    public bool playerIn = false;
+    public GameObject shape;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        shape.GetComponent<TracerFollow>().enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //print("hello");
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        //print("Object Entered");
+        if (other.gameObject.tag == "Player")
+        {
+            print("Player is Inbounds");
+            playerIn = true;
+            shape.GetComponent<TracerFollow>().enabled = true;
+            //shape.GetComponent<LineRenderer>().enabled = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            print("Player Left");
+            playerIn = false;
+            shape.GetComponent<TracerFollow>().enabled = false;
+            //shape.GetComponent<LineRenderer>().enabled = true;
+        }
+    }
+}
