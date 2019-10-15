58a1f3266a5e51119d1b751672760c2e8d50c8eb
amandeep <amandeepmalhi21@hotmail.com>
2019-10-14 23:01:30 -0700

    created basis for fireblast trigger

---
+++ Assets/Scripts/ShootFire.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootFire : MonoBehaviour
+{
+
+    public GameObject fireball;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+
+            Instantiate(fireball, transform.position + new Vector3(0,0,0.5f), transform.rotation);
+        }
+
+    }
+
+    
+}
